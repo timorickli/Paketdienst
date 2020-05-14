0c42784ec6661aa95a767f0c7e8327db5364e30d
Timo Rickli <timo.rickli@ict-campus.net>
2020-05-14 11:21:58 +0200

    Activity

--- app/src/main/java/net/ictcampus/paketdienst/inventoryActivity.java
+++ app/src/main/java/net/ictcampus/paketdienst/inventoryActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class startActivity extends AppCompatActivity {
+public class inventoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_inventory);
     }
 }
