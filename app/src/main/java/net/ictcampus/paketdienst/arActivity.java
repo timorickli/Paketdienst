5d0553a50cbb2fa0e3e3642152162e695a99d3a3
Timo Rickli <timo.rickli@ict-campus.net>
2020-05-14 10:58:33 +0200

    Activity

---
+++ app/src/main/java/net/ictcampus/paketdienst/arActivity.java
@@ -0,0 +1,14 @@
+package net.ictcampus.paketdienst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class arActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar);
+    }
+}
---
+++ app/src/main/java/net/ictcampus/paketdienst/mapActivity.java
@@ -0,0 +1,14 @@
+package net.ictcampus.paketdienst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
